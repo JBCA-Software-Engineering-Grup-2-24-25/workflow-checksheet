eb78dd06ce4f8db183b0b7c963ddcce0ce38bba9
David Surya <suryadavid@ymail.com>
2025-05-10 01:07:03 +0700

    modify: user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->foreignId('role_id')->nullable()->index();
+            $table->foreignId('author_id')->nullable()->index();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
